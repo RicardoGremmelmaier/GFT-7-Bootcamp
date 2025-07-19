f58eca8f6472226e1cbd9917212769b007047f05
Ricardo Marthus Gremmelmaier <ricardomaier10@gmail.com>
2025-07-18 21:10:56 -0300

    first java project

---
+++ Introduction/Hello/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Bem vindo, " + args[0]);
+        System.out.println("Você está programando em " + args[1]);
+    }
+}
